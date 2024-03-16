0fed55738eb5fe5d87b360a4fb1ed6586c95f256
Nick <nicholasconnors@hotmail.ca>
2024-03-15 21:25:51 -0400

    Add more hatchling dialogue, fix meditation breaking, respawn better

---
+++ PlayAsGabbro/MeditationDialogue.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PlayAsGabbro;
+
+internal static class MeditationDialogue
+{
+    public static void Setup()
+    {
+        GlobalMessenger.AddListener("ExitConversation", OnExitConversation);
+    }
+
+    private static void OnExitConversation()
+    {
+        if (DialogueConditionManager.SharedInstance.GetConditionState("HatchlingMeditate"))
+        {
+            Locator.GetDeathManager().KillPlayer(DeathType.Meditation);
+        }
+    }
+}
--- PlayAsGabbro/PlayAsGabbro.cs
+++ PlayAsGabbro/PlayAsGabbro.cs
@@ -1,6 +1,7 @@
 ﻿using OWML.Common;
 using OWML.ModHelper;
 using System;
+using System.Collections;
 using UnityEngine;
 
 namespace PlayAsGabbro
@@ -26,47 +27,63 @@ namespace PlayAsGabbro
             var newHorizons = ModHelper.Interaction.TryGetModApi<INewHorizons>("xen.NewHorizons");
             newHorizons.LoadConfigs(this);
 
+            MeditationDialogue.Setup();
+
             // Example of accessing game code.
             LoadManager.OnCompleteSceneLoad += (scene, loadScene) =>
             {
                 if (loadScene == OWScene.SolarSystem || loadScene == OWScene.EyeOfTheUniverse)
                 {
-                    FireOnNextUpdate(() =>
-                    {
-                        Locator.GetPlayerSuit().SuitUp(false, true, true);
-                        SkinReplacer.ReplaceSkin(GameObject.Find("Player_Body"), true);
-                        
-                        // Spawn player
-                        var island = GameObject.Find("GabbroIsland_Body");
-                        var player = Locator.GetPlayerBody();
-
-                        var worldPosition = island.transform.TransformPoint(new Vector3(38.325f, 18.537f, 6.619f));
-                        var worldRotation = island.transform.rotation;
-                        player.WarpToPositionRotation(worldPosition, worldRotation);
-                        player.SetVelocity(island.GetAttachedOWRigidbody().GetVelocity());
-                        var campfire = island.GetComponentInChildren<Campfire>();
-                        player.transform.LookAt(campfire.transform, island.transform.up);
-
-                        // Spawn ship
-                        var ship = Locator.GetShipBody();
-                        var shipWorldPosition = island.transform.position + player.transform.forward * 80f;
-
-                        // Move position into the water
-                        var giantsDeep = Locator.GetAstroObject(AstroObject.Name.GiantsDeep);
-                        var relativePosition = giantsDeep.transform.InverseTransformPoint(shipWorldPosition);
-                        var inWaterRelativePosition = relativePosition.normalized * 500f;
-                        var finalShipWorldPosition = giantsDeep.transform.TransformPoint(inWaterRelativePosition);
-
-                        ship.WarpToPositionRotation(finalShipWorldPosition, worldRotation);
-                        ship.SetVelocity(island.GetAttachedOWRigidbody().GetVelocity());
-
-                        if (!Physics.autoSyncTransforms)
-                        {
-                            Physics.SyncTransforms();
-                        }
-                    });
+                    StartCoroutine(SpawnCoroutine());
                 }
             };
+        }
+
+        private IEnumerator SpawnCoroutine()
+        {
+            yield return new WaitForFixedUpdate();
+
+            Locator.GetPlayerSuit().SuitUp(false, true, true);
+            SkinReplacer.ReplaceSkin(GameObject.Find("Player_Body"), true);
+
+            for (int i = 0; i < 10; i++)
+            {
+                OnPlayerSpawn();
+                yield return new WaitForFixedUpdate();
+            }
+
+        }
+
+        private void OnPlayerSpawn()
+        {
+            // Spawn player
+            var island = GameObject.Find("GabbroIsland_Body");
+            var player = Locator.GetPlayerBody();
+
+            var worldPosition = island.transform.TransformPoint(new Vector3(38.325f, 18.537f, 6.619f));
+            var worldRotation = island.transform.rotation;
+            player.WarpToPositionRotation(worldPosition, worldRotation);
+            player.SetVelocity(island.GetAttachedOWRigidbody().GetVelocity());
+            var campfire = island.GetComponentInChildren<Campfire>();
+            player.transform.LookAt(campfire.transform, island.transform.up);
+
+            // Spawn ship
+            var ship = Locator.GetShipBody();
+            var shipWorldPosition = island.transform.position + player.transform.forward * 80f;
+
+            // Move position into the water
+            var giantsDeep = Locator.GetAstroObject(AstroObject.Name.GiantsDeep);
+            var relativePosition = giantsDeep.transform.InverseTransformPoint(shipWorldPosition);
+            var inWaterRelativePosition = relativePosition.normalized * 500f;
+            var finalShipWorldPosition = giantsDeep.transform.TransformPoint(inWaterRelativePosition);
+
+            ship.WarpToPositionRotation(finalShipWorldPosition, worldRotation);
+            ship.SetVelocity(island.GetAttachedOWRigidbody().GetVelocity());
+
+            if (!Physics.autoSyncTransforms)
+            {
+                Physics.SyncTransforms();
+            }
         }
     }
 }
